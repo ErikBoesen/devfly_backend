f03f15b177df08e04fd4014041ea28c05fa3f7a5
ErikBoesen <me@erikboesen.com>
2022-10-30 19:34:44 -0400

    Add blueprints at sub-urls

--- app/__init__.py
+++ app/__init__.py
@@ -14,5 +14,5 @@ migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 
 from app import routes, models, errors, api, auth, util
-app.register_blueprint(api.api_bp, name='api')
-app.register_blueprint(auth.auth_bp, name='auth')
+app.register_blueprint(api.api_bp, url_prefix='/api', name='api')
+app.register_blueprint(auth.auth_bp, url_prefix='/auth', name='auth')
